4f82b1f5c70b858505f4269d10bff9f4c02f0f7b
mukoyadariu <mukoya.darius@student.moringaschool.com>
2023-08-15 07:18:08 +0300

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print("Hello, " + name + "!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print("Hello, " + name + "!")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
-    pass
+    return number / 2
+
+greet_programmer()
+greet("Guido")
+greet_with_default()
+greet_with_default("Mike")
+result=add(1, 2)
+print(result)
+halved_value=halve(40)
+print(halved_value)
